0fcdff585fe01fd623d7ec3491ea5dcdb6f9a8f9
Lyubov Rodina <kreizsasha@gmail.com>
2023-07-13 14:25:17 +0300

    add new tests to test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,17 @@
-from gendiff import generate_diff, gen_js_bool
+from gendiff import generate_diff, gen_js_bool, get_converted_file
 import pytest
 
 
 path = 'tests/fixtures/result_json.txt'
-first_file = 'tests/fixtures/file1.json'
-second_file = 'tests/fixtures/file2.json'
+fst_json = 'tests/fixtures/file1.json'
+scnd_json = 'tests/fixtures/file2.json'
+fst_yaml = 'tests/fixtures/file1.yml'
+scnd_yaml = 'tests/fixtures/file2.yaml'
 
 
-@pytest.mark.parametrize('file1, file2, excepted', [(first_file, second_file, path)])
+@pytest.mark.parametrize('file1, file2, excepted', [(fst_json, scnd_json, path),
+                                                    (fst_yaml, scnd_yaml, path)
+                                                    ])
 def test_generate_diff(file1, file2, excepted):
     with open(excepted, 'r') as excepted_result:
         result = excepted_result.read()
@@ -19,3 +23,10 @@ def test_gen_js_bool():
     assert gen_js_bool('') == ''
     assert gen_js_bool(10) == 10
     assert gen_js_bool('false') == 'false'
+
+
+@pytest.mark.parametrize('file', [fst_json, scnd_json,
+                                  fst_yaml, scnd_yaml
+                                  ])
+def test_get_converted_file(file):
+    assert isinstance(get_converted_file(file), dict)
