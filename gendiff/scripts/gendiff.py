f45ee555c5e310ebcf83c6db4861788930a8a579
Lyubov Rodina <kreizsasha@gmail.com>
2023-07-13 14:27:28 +0300

    remove parsing from script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 
 
-import argparse
-from gendiff import generate_diff
-
-
-parser = argparse.ArgumentParser(description='Compares two configuration files'
-                                             ' and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
-args = parser.parse_args()
+from gendiff import generate_diff, args
 
 
 def main():
+    args
     print(generate_diff(args.first_file, args.second_file))
 
 
