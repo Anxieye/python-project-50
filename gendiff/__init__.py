c97f8cdbd94ec7c16e04caeaf3b57f66fb900416
Lyubov Rodina <kreizsasha@gmail.com>
2023-07-13 14:28:33 +0300

    add some dependencies

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,5 @@
-from gendiff.gendiff import generate_diff, gen_js_bool
+from gendiff.gendiff import generate_diff, gen_js_bool, get_converted_file
+from gendiff.cli import args
 
 
-__all__ = ('generate_diff', 'gen_js_bool')
+__all__ = ('generate_diff', 'gen_js_bool', 'get_converted_file', 'args')
