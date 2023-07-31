31173908e571898ccb373a4ccbbdb67b7ed2f845
Lyubov Rodina <kreizsasha@gmail.com>
2023-07-31 15:38:40 +0300

    add formatter

---
+++ gendiff/formatters/__init__.py
@@ -0,0 +1,8 @@
+from gendiff.formatters.js_formatter import stylish, gen_js_bool
+from gendiff.formatters.js_formatter import gen_json_string
+
+
+__all__ = ('gen_js_bool',
+           'stylish',
+           'gen_json_string'
+           )
