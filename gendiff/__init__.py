5e5e9d8f27cdfccc954df765dd85c28e9e55bf0e
Lyubov Rodina <kreizsasha@gmail.com>
2023-07-31 15:35:20 +0300

    updade __init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,11 @@
-from gendiff.gendiff import generate_diff, gen_js_bool, get_converted_file
+from gendiff.gendiff import generate_diff, get_converted_file
+from gendiff.formatters import stylish, gen_js_bool
+from gendiff.formatters import gen_json_string
 
 
-__all__ = ('generate_diff', 'gen_js_bool', 'get_converted_file')
+__all__ = ('generate_diff',
+           'gen_js_bool',
+           'get_converted_file',
+           'stylish',
+           'gen_json_string'
+           )
