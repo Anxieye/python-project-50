42daf3ee97e0aecd760e4824766338c971e57a37
Lyubov Rodina <kreizsasha@gmail.com>
2023-08-09 13:32:53 +0300

    update stylish formatter

---
+++ gendiff/formatters/stylish_formatter.py
@@ -0,0 +1,143 @@
+from gendiff.formatters.to_js import gen_js_bool
+
+
+def gen_json_string(dict_value, depth, special):
+    """
+    This function generates a string from a key's value that is a dictionary.
+    It is used to traverse key values that the function 'diff' did not traverse
+    due to the fact that the key was in one of the two files
+    or when the key was changed, the value was a dictionary.
+    """
+    string = ''
+    spaces = 4
+    step = spaces * depth - 2
+    space = ' ' * step
+
+    for key, value in dict_value.items():
+        value = gen_js_bool(value)
+        if isinstance(value, dict):
+            string += f'{space}{special}{key}: '
+            string += f'{gen_json_string(value, depth + 1, special)}\n'
+        else:
+            string += f'{space}{special}{key}: {value}\n'
+
+    return '{\n' + string + (' ' * spaces * (depth - 1)) + '}'
+
+
+def stylish(diff: dict) -> str:
+    """
+    This function is formatter for dict type from python.
+    Generates a string that looks like an object type from json files
+    """
+    def inner(diff, depth):
+        string = ''
+        spaces = 4
+        step = spaces * depth - 2
+        space = ' ' * step
+        immut = '  '
+        for key in diff:
+            value = gen_js_bool(diff.get(key).get('value'))
+            state = diff.get(key).get('state')
+            if state == 'added':
+                string += gen_added_property(value,
+                                             space,
+                                             key,
+                                             depth,
+                                             immut
+                                             )
+            elif state == 'deleted':
+                string += gen_deleted_property(value,
+                                               space,
+                                               key,
+                                               depth,
+                                               immut
+                                               )
+            elif state == 'not_changed':
+                string += gen_not_changed_propety(value,
+                                                  space,
+                                                  key,
+                                                  depth,
+                                                  immut
+                                                  )
+            elif state == 'changed':
+                old = gen_js_bool(diff.get(key).get('old_value'))
+                new = gen_js_bool(diff.get(key).get('new_value'))
+                string += gen_changed_property(new,
+                                               old,
+                                               space,
+                                               key,
+                                               depth,
+                                               immut
+                                               )
+            else:
+                string += f'{space}{immut}{key}: '
+                string += f'{inner(value, depth + 1)}\n'
+        return '{\n' + string + (' ' * spaces * (depth - 1)) + '}'
+    return inner(diff, 1)
+
+
+def gen_added_property(value, space, key, depth, immut):
+    """
+    This function creates a string which contains an added property
+    """
+    string = ''
+    added = '+ '
+    if isinstance(value, dict):
+        string += f'{space}{added}{key}: '
+        string += f'{gen_json_string(value, depth + 1, immut)}\n'
+    else:
+        string += f'{space}{added}{key}: {value}\n'
+    return string
+
+
+def gen_deleted_property(value, space, key, depth, immut):
+    """
+    This function creates a string which contains a deleted property
+    """
+    deleted = '- '
+    string = ''
+    if isinstance(value, dict):
+        string += f'{space}{deleted}{key}: '
+        string += f'{gen_json_string(value, depth + 1, immut)}\n'
+    else:
+        string += f'{space}{deleted}{key}: {value}\n'
+    return string
+
+
+def gen_not_changed_propety(value, space, key, depth, immut):
+    """
+    This function creates a string which contains a not changed property
+    """
+    string = ''
+    if isinstance(value, dict):
+        string += f'{space}{immut}{key}: '
+        string += f'{gen_json_string(value, depth + 1, immut)}\n'
+    else:
+        string += f'{space}{immut}{key}: {value}\n'
+    return string
+
+
+def gen_changed_property(new, old, space, key, depth, immut):
+    """
+    This function creates a string which contains a changed property
+    """
+    string = ''
+    added = '+ '
+    deleted = '- '
+    if isinstance(old, dict) and not isinstance(new, dict):
+        string += f'{space}{deleted}{key}: '
+        string += f'{gen_json_string(old, depth + 1, immut)}\n'
+        string += f'{space}{added}{key}: {new}\n'
+    elif isinstance(new, dict) and not isinstance(old, dict):
+        string += f'{space}{deleted}{key}: {old}\n'
+        string += f'{space}{added}{key}: '
+        string += f'{gen_json_string(new, depth + 1, immut)}\n'
+    elif isinstance(old, dict) and isinstance(new, dict):
+        string += f'{space}{deleted}{key}: '
+        string += f'{gen_json_string(old, depth + 1, immut)}\n'
+        string += f'{space}{added}{key}: '
+        string += f'{gen_json_string(new, depth + 1, immut)}\n'
+    else:
+        string += f'{space}{deleted}{key}: {old}\n'
+        string += f'{space}{added}{key}: {new}\n'
+    return string
