28b729b49d550db40ee15313ed31f2ce363d500f
Lyubov Rodina <kreizsasha@gmail.com>
2023-08-09 13:35:07 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,29 +1,38 @@
-from gendiff import generate_diff, gen_js_bool, get_converted_file
+from gendiff import generate_diff, get_converted_file
+from gendiff.formatters import gen_js_bool
 import pytest
 
 
 flat = 'tests/fixtures/result_json.txt'
 tree = 'tests/fixtures/result_tree.txt'
+plain = 'tests/fixtures/plain_result.txt'
 fst_json = 'tests/fixtures/file1.json'
 scnd_json = 'tests/fixtures/file2.json'
 fst_yaml = 'tests/fixtures/file1.yml'
 scnd_yaml = 'tests/fixtures/file2.yaml'
-js_tree1 = 'tests/fixtures/tree1.json'
-js_tree2 = 'tests/fixtures/tree2.json'
-yml_tree1 = 'tests/fixtures/tree1.yaml'
-yml_tree2 = 'tests/fixtures/tree2.yml'
+js_1 = 'tests/fixtures/tree1.json'
+js_2 = 'tests/fixtures/tree2.json'
+yml_1 = 'tests/fixtures/tree1.yaml'
+yml_2 = 'tests/fixtures/tree2.yml'
 
 
-@pytest.mark.parametrize('file1, file2, excepted', [(fst_json, scnd_json, flat),
-                                                    (fst_yaml, scnd_yaml, flat),
-                                                    (js_tree1, js_tree2, tree),
-                                                    (yml_tree1, yml_tree2, tree)
-                                                    ]
+@pytest.mark.parametrize('file1, file2, excepted, format',
+                         [
+                          (fst_json, scnd_json, flat, 'stylish'),
+                          (fst_yaml, scnd_yaml, flat, 'stylish'),
+                          (fst_json, scnd_yaml, flat, 'stylish'),
+                          (js_1, js_2, tree, 'stylish'),
+                          (js_1, yml_2, tree, 'stylish'),
+                          (yml_1, yml_2, tree, 'stylish'),
+                          (js_1, js_2, plain, 'plain'),
+                          (js_1, yml_2, plain, 'plain'),
+                          (yml_1, yml_2, plain, 'plain')
+                          ]
                          )
-def test_generate_diff(file1, file2, excepted):
+def test_generate_diff(file1, file2, excepted, format):
     with open(excepted, 'r') as excepted_result:
         result = excepted_result.read()
-        assert generate_diff(file1, file2) == result
+        assert generate_diff(file1, file2, format) == result
 
 
 def test_gen_js_bool():
@@ -36,8 +45,8 @@ def test_gen_js_bool():
 
 @pytest.mark.parametrize('file', [fst_json, scnd_json,
                                   fst_yaml, scnd_yaml,
-                                  js_tree1, js_tree2,
-                                  yml_tree1, yml_tree2
+                                  js_1, js_2,
+                                  yml_1, yml_2
                                   ]
                          )
 def test_get_converted_file(file):
