5b7f5bc65408fd5402830a6cafaf03782672b928
Lyubov Rodina <kreizsasha@gmail.com>
2023-08-09 13:34:16 +0300

    update converter to bool

---
+++ gendiff/formatters/to_js.py
@@ -0,0 +1,12 @@
+def gen_js_bool(value):
+    """
+    This function generates a string like in json files
+    from python bool data type and NoneType.
+    Example: True = 'true', None = 'null'
+    """
+    if isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return 'null'
+    else:
+        return value
